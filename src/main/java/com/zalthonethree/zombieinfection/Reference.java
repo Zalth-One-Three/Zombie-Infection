e0c835d9235a896ef905ed030a84d3d10eded237
61352151511 <cassboys@hotmail.com>
2015-02-16 21:29:11 -0400

    The process... Has started

--- src/main/java/com/zalthonethree/zombieinfection/Reference.java
+++ src/main/java/com/zalthonethree/zombieinfection/Reference.java
@@ -4,7 +4,7 @@ public class Reference /*extends EntityDragon*/ {
 	public static final String MOD_ID = "ZombieInfection";
 	public static final String MOD_NAME = "Zombie Infection";
 	public static final String MINECRAFT_VERSION = "1.8";
-	public static final String VERSION = "2.2.0";
+	public static final String VERSION = "2.3.0";
 	public static final String CLIENT_PROXY = "com.zalthonethree.zombieinfection.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.zalthonethree.zombieinfection.proxy.ServerProxy";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";

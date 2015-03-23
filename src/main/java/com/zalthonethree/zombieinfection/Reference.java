0d38f85076e9dbe8b2cc8da8ff16699aa3952bec
61352151511 <cassboys@hotmail.com>
2015-03-23 12:09:21 -0300

    Test Build

--- src/main/java/com/zalthonethree/zombieinfection/Reference.java
+++ src/main/java/com/zalthonethree/zombieinfection/Reference.java
@@ -4,7 +4,7 @@ public class Reference /*extends EntityDragon*/ {
 	public static final String MOD_ID = "ZombieInfection";
 	public static final String MOD_NAME = "Zombie Infection";
 	public static final String MINECRAFT_VERSION = "1.7.10";
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String CLIENT_PROXY = "com.zalthonethree.zombieinfection.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.zalthonethree.zombieinfection.proxy.ServerProxy";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";

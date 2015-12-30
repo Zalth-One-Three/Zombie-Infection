c21267766746d0f5ab2b48ee1e01e61b6a000837
61352151511 <cassboys@hotmail.com>
2015-12-30 07:05:57 -0400

    1.8.9

--- src/main/java/com/zalthonethree/zombieinfection/Reference.java
+++ src/main/java/com/zalthonethree/zombieinfection/Reference.java
@@ -3,7 +3,7 @@ package com.zalthonethree.zombieinfection;
 public class Reference {
 	public static final String MOD_ID = "ZombieInfection";
 	public static final String MOD_NAME = "Zombie Infection";
-	public static final String MINECRAFT_VERSION = "1.8.8";
+	public static final String MINECRAFT_VERSION = "1.8.9";
 	public static final String VERSION = "3.0.0";
 	public static final String CLIENT_PROXY = "com.zalthonethree.zombieinfection.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.zalthonethree.zombieinfection.proxy.ServerProxy";

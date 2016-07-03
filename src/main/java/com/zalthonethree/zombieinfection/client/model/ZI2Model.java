e2c6994000482b7c78e6fcf4a38f1275698ecfd8
Six-One-Three <mitchell.casavechia@outlook.com>
2016-07-03 11:00:55 -0300

    More work on ZI2Model

--- src/main/java/com/zalthonethree/zombieinfection/client/model/ZI2Model.java
+++ src/main/java/com/zalthonethree/zombieinfection/client/model/ZI2Model.java
@@ -2,21 +2,32 @@ package com.zalthonethree.zombieinfection.client.model;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.block.model.ModelRotation;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.IModelCustomData;
+import net.minecraftforge.client.model.IPerspectiveAwareModel;
 import net.minecraftforge.client.model.IRetexturableModel;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.common.model.TRSRTransformation;
 
 import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
@@ -25,10 +36,10 @@ import com.zalthonethree.zombieinfection.block.BlockBase;
 import com.zalthonethree.zombieinfection.utility.LogHelper;
 
 public class ZI2Model implements IModelCustomData {
-	private final BlockBase block;
+	private final ResourceLocation resourceLocation;
 	
-	public ZI2Model(BlockBase block) {
-		this.block = block;
+	public ZI2Model(ResourceLocation resourceLocation) {
+		this.resourceLocation = resourceLocation;
 	}
 	
 	@Override public Collection<ResourceLocation> getDependencies() {
@@ -36,61 +47,50 @@ public class ZI2Model implements IModelCustomData {
 	}
 	
 	@Override public Collection<ResourceLocation> getTextures() {
-		return ImmutableSet.of(block.getTexture());
+		return ImmutableSet.of(resourceLocation); // TODO Convert the model location to a texture location.
 	}
 	
-	@Override IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
-		ImmutableMap.Builder<String, TextureAtlasSprite> builder = ImmutableMap.builder();
-		builder.put(ModelLoader.White.LOCATION.toString(), ModelLoader.White.INSTANCE);
-		TextureAtlasSprite missing = bakedTextureGetter.apply(new ResourceLocation("missingno"));
-		for (Map.Entry<String, Material> e : matLib.materials.entrySet()) {
-			builder.put(e.getKey(), bakedTextureGetter.apply(e.getValue().getTexture().getTextureLocation()))
-		}
-		builder.put("missingno", missing);
-		return new ZI2BakedModel(this, state, format, builder.build());
+	@Override public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
+		ImmutableMap<TransformType,TRSRTransformation> map = IPerspectiveAwareModel.MapWrapper.getTransforms(state);
+		return new ZI2BakedModel(state, map, format, bakedTextureGetter);
 	}
 	
-	public MaterialLibrary getMatLib() {
-		return this.matLib;
+	@Override public IModelState getDefaultState() {
+		return ModelRotation.X0_Y0;
 	}
 	
-	@Override public IModel process(ImmutableMap<String, String> customData) {
-		ZI2Model ret = new ZI2Model(this.matLib, this.modelLocation, new CustomData(this.customData, customData));
-		return ret;
-	}
-	
-	@Override public IModel retexture(ImmutableMap<String, String> textures) {
-		ZI2Model ret = new ZI2Model(this.matLib.makeLibWithReplacements(textures), this.modelLocation, this.customData);
-		return ret;
-	}
-	
-	static class CustomData {
-		public boolean ambientOcclusion = true;
-		public boolean gui3d = true;
-		public boolean flipV = false;
+	public static enum ZI2Loader implements ICustomModelLoader {
+		INSTANCE;
 		
-		public CustomData(CustomData parent, ImmutableMap<String, String> customData) {
-			this.ambientOcclusion = parent.ambientOcclusion;
-			this.gui3d = parent.gui3d;
-			this.flipV = parent.flipV;
-			this.process(customData);
+		public void onResourceManagerReload(IResourceManager resourceManager) {}
+		
+		public boolean accepts(ResourceLocation modelLocation) {
+			return modelLocation.getResourceDomain().contains("zombieinfection");
 		}
 		
-		public CustomData() {}
+		public IModel loadModel(ResourceLocation modelLocation) {
+			return new ZI2Model(modelLocation);
+		}
+	}
+	
+	private static final class ZI2BakedModel implements IPerspectiveAwareModel {
+		private static final int[] x = {0, 0, 1, 1};
+		private static final int[] z = {0, 1, 1, 0};
 		
-		public void process(ImmutableMap<String, String> customData) {
-			for (Map.Entry<String, String> e : customData.entrySet()) {
-				if (e.getKey().equals("ambient"))
-					this.ambientOcclusion = Boolean.valueOf(e.getValue());
-				else if (e.getKey().equals("gui3d"))
-					this.gui3d = Boolean.valueOf(e.getValue());
-				else if (e.getKey().equals("flip-v"))
-					this.flipV = Boolean.valueOf(e.getValue());
+		private final LoadingCache<Long, ZI2BakedModel> modelCache = CacheBuilder.newBuilder().maximumSize(200).build(new CacheLoader<Long, ZI2BakedModel>() {
+			public ZI2BakedModel load(Long key) throws Exception {
+				return new ZI2BakedModel(transformation, transforms, format);
 			}
-		}
-	}
-	
-	public static class Parser {
-		private static final Pattern WHITE_SPACE = Pattern.compile("\\s+");
+			
+			private final Optional<TRSRTransformation> transformation;
+			private final ImmutableMap<TransformType, TRSRTransformation> transforms;
+			private final VertexFormat format;
+			private final TextureAtlasSprite texture;
+			private final EnumMap<EnumFacing, List<BakedQuad>> faceQuads;
+			
+			public ZI2BakedModel(Optional<TRSRTransformation> transformation, ImmutableMap<TransformType, TRSRTransformation> transforms, TextureAtlasSprite texture) {
+				
+			}
+		});
 	}
 }
